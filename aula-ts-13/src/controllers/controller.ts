36da7869e6ed746fd178c478f09c4e47b2a10c5c
Luca-Panza <luca.panza.007@gmail.com>
2023-09-17 08:18:51 -0300

    feat: lesson 13

---
+++ aula-ts-13/src/controllers/controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+function getInfo(req: Request, res: Response) {
+  res.send("Hello!");
+}
+
+const controller = { getInfo };
+
+export default controller;
